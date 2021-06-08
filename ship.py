0fc6f9191e4c29f823de117a09d3f3615c730eba
NibbaNaeNae <71708229+NibbaNaeNae@users.noreply.github.com>
2021-06-08 19:51:50 +0100

    Update ship.py

--- ship.py
+++ ship.py
@@ -10,11 +10,19 @@ class Ship:
         self.screen_rect = ai_game.screen.get_rect()
 
         # load the ship image and get its rect
-        self.image = pygame.image.load('ship.bmp') # the original location for ship.bmp is images/shit.bmp, but I cant add folders
+        self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
 
         # start each new ship at the bottom center of the screen
         self.rect.midbottom = self.screen_rect.midbottom
+
+        # movement flag
+        self.moving_right = False
+
+    def update(self):
+        """Update the ships position based on the movement flag"""
+        if self.moving_right:
+            self.rect.x += 1
 
     def blitme(self):
         """draw the ship at its current location"""
