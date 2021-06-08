5fbc5dcf39988bbaf19f2f45752e7331e9bbe83f
NibbaNaeNae <71708229+NibbaNaeNae@users.noreply.github.com>
2021-06-08 19:51:24 +0100

    Update alien_invasion.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -21,9 +21,9 @@ class AlienInvasion:
 
     def run_game(self):
         """Start the main loop for the game"""
-
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
 
     def _check_events(self):
@@ -31,8 +31,14 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship_moving_right = True
+                elif event.type == pygame.KEYUP:
+                    if event.key == pygame.K_RIGHT:
+                        self.ship_moving_right = False
 
-    def update_screen(self):
+    def _update_screen(self):
         """Update images on the screen, and flip to the new screen"""
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
@@ -44,3 +50,4 @@ if __name__ == '__main__':
     # make a game instance, and run the game
     ai = AlienInvasion()
     ai.run_game()
+
